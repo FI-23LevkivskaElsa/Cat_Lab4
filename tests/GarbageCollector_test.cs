a39f270bd53408cbb49ba258dbdcc7615485f4e9
Elsa <elzlevk-ipt25@lll.kpi.ua>
2024-05-26 20:47:34 +0300

    first commit

---
+++ tests/GarbageCollector_test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace tests;
+
+[TestFixture]
+class GarbageCollectorTest
+{
+    private const int maxGarbage = 1000;
+
+    [Test]
+    static void Main()
+    {
+        GarbageCollectorTest.MakeSomeGarbage();
+        var before = GC.GetTotalMemory(false);
+
+        GC.Collect();
+        var after = GC.GetTotalMemory(true);
+        Assert.That(before > after, Is.EqualTo(true));
+    }
+
+    static void MakeSomeGarbage()
+    {
+        Version vt;
+
+        for (int i = 0; i < maxGarbage; i++)
+        {
+            vt = new Version();
+        }
+    }
+}
---
+++ tests/ValueTypes_test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace tests;
+
+[TestFixture]
+public sealed class ValueTypeTest
+{
+    [Test]
+    public void AddingNumbTest()
+    {
+        var val_1 = 7;
+        var val_2 = 9;
+        var value = val_1 + val_2;
+        Assert.That(value, Is.EqualTo(16));
+    }
+}
